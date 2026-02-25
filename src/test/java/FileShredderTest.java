1ec66b844d14439f60f589a43bfb7d5ae18ad4c1
Sverdlov Semen <semensverdlov4@gmail.com>
2026-02-25 16:15:41 +0700

    Step 3: Added test for shred method interface (fails)

--- src/test/java/FileShredderTest.java
+++ src/test/java/FileShredderTest.java
@@ -8,4 +8,12 @@ public class FileShredderTest {
         FileShredder shredder = new FileShredder();
         assertNotNull(shredder);
     }
+
+    @Test
+    void testShredMethodInterface() {
+        FileShredder shredder = new FileShredder();
+        // Мы хотим, чтобы метод принимал путь и возвращал true в случае успеха
+        boolean result = shredder.shred("non_existent_file.txt");
+        assertFalse(result, "Should return false if file does not exist");
+    }
 }
